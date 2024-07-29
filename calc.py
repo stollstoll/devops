3d448eda40c98689ebf0f1a81416aca6a66ec9db
stollstoll <stoll.stoll@gmail.com>
2024-07-29 18:47:08 +0930

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 15:33:43 2024
+
+@author: Work
+"""
+#my_calculator
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
+
+def square(x, y):
+    'square function'
+    return square(x, y)     #return 'x' to the power of 'y' 
+
+
