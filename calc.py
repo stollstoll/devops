f97a344251b3faaf497c9544236696baf3c0a185
stollstoll <stoll.stoll@gmail.com>
2024-07-29 20:09:49 +0930

    Update calc.py

--- calc.py
+++ calc.py
@@ -16,12 +16,12 @@ def subtract(x, y):
     return x - y
 
 
-def multiply(x, y):
+def multiply(x, y): 
     """Multiply Function"""
     return x * y
 
 
-def divide(x, y):
+def divide(x, y): 
     """Divide Function"""
     if y == 0:
         raise ValueError('Can not divide by zero!')
